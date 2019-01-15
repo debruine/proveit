95d4842b204164308eea668a5da759d38e9dcfda
debruine <debruine@gmail.com>
2019-01-15 15:51:29 +0000

    comment out graphs made with ggally

--- lmer_vs_anova/R/plot_funcs.R
+++ lmer_vs_anova/R/plot_funcs.R
@@ -34,39 +34,39 @@ plot_dat <- function(dat, grand_i = 0, view = c("violin", "boxplot"), grp = "") 
 
 # plot random effects by subject for model and data
 plot_ranef_sub <- function(mod, dat) {
-  sub <- dat %>%
-    group_by(sub_id, sub_i, sub_version_slope) %>%
-    summarise()
-  
-  ranef(mod)$sub_id %>%
-    as_tibble(rownames = "sub_id") %>%
-    rename(mod_i = `(Intercept)`,
-           mod_version_slope = stim_version.code) %>%
-    mutate(sub_id = as.integer(sub_id)) %>%
-    left_join(sub, by = "sub_id") %>%
-    select(mod_i, sub_i, mod_version_slope,  sub_version_slope) %>%
-    GGally::ggpairs(lower = list(continuous = "smooth"),
-                    progress = FALSE)
+  # sub <- dat %>%
+  #   group_by(sub_id, sub_i, sub_version_slope) %>%
+  #   summarise()
+  # 
+  # ranef(mod)$sub_id %>%
+  #   as_tibble(rownames = "sub_id") %>%
+  #   rename(mod_i = `(Intercept)`,
+  #          mod_version_slope = stim_version.code) %>%
+  #   mutate(sub_id = as.integer(sub_id)) %>%
+  #   left_join(sub, by = "sub_id") %>%
+  #   select(mod_i, sub_i, mod_version_slope,  sub_version_slope) %>%
+  #   GGally::ggpairs(lower = list(continuous = "smooth"),
+  #                   progress = FALSE)
 }
 
 # plot random effects by stimuli for model and data
 plot_ranef_stim <- function(mod, dat) {
-  stim <- dat %>%
-    group_by(stim_id, stim_i, stim_version_slope, stim_cond_slope, stim_cond_version_slope) %>%
-    summarise()
-  
-  ranef(mod)$stim_id %>%
-    as_tibble(rownames = "stim_id") %>%
-    rename(mod_i = `(Intercept)`,
-           mod_version_slope = stim_version.code,
-           mod_cond_slope = sub_cond.code,
-           mod_cond_version_slope = `stim_version.code:sub_cond.code`) %>%
-    mutate(stim_id = as.integer(stim_id)) %>%
-    left_join(stim, by = "stim_id") %>%
-    select(mod_i, stim_i, 
-           mod_version_slope, stim_version_slope, 
-           mod_cond_slope, stim_cond_slope, 
-           mod_cond_version_slope, stim_cond_version_slope) %>%
-    GGally::ggpairs(lower = list(continuous = "smooth"),
-                    progress = FALSE)
+  # stim <- dat %>%
+  #   group_by(stim_id, stim_i, stim_version_slope, stim_cond_slope, stim_cond_version_slope) %>%
+  #   summarise()
+  # 
+  # ranef(mod)$stim_id %>%
+  #   as_tibble(rownames = "stim_id") %>%
+  #   rename(mod_i = `(Intercept)`,
+  #          mod_version_slope = stim_version.code,
+  #          mod_cond_slope = sub_cond.code,
+  #          mod_cond_version_slope = `stim_version.code:sub_cond.code`) %>%
+  #   mutate(stim_id = as.integer(stim_id)) %>%
+  #   left_join(stim, by = "stim_id") %>%
+  #   select(mod_i, stim_i, 
+  #          mod_version_slope, stim_version_slope, 
+  #          mod_cond_slope, stim_cond_slope, 
+  #          mod_cond_version_slope, stim_cond_version_slope) %>%
+  #   GGally::ggpairs(lower = list(continuous = "smooth"),
+  #                   progress = FALSE)
 }
